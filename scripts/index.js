8044b80ade00b5d1b57bc60335fb81ace2ccb766
Mikhail Chernyshev <lishainik@gmail.com>
2025-03-27 13:50:45 +0300

    initial

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+
+function removeCard() {}
+
+function createCard(element) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardList = document.querySelector(".places__list");
+
+  const cardElement = cardTemplate.cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__image").src = element.link;
+  cardElement.querySelector(".card__title").textContent = element.name;
+
+  deleteButton.addEventListener("click", function () {
+    const card = deleteButton.closest(".card");
+    card.remove();
+  });
+
+  cardList.append(cardElement);
+}
+
+initialCards.forEach(createCard);
